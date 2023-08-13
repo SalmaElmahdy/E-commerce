00532c19fcc42c3d21bcd2618db271d854f7ded6
Salma <samelmahdy077@gmail.com>
2023-08-13 22:24:01 +0300

    added store page with products  and get store products by category slug or name in url request

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from ecommerce import views
 from django.conf.urls.static import static
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('store/', include('store.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
